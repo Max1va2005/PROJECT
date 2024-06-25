cebda6a8d233acf5121661b9bb520713bc89a35a
User <User@WIN-A9UR2BJ38MV>
2024-06-25 11:32:56 +0300

    Add project files.

---
+++ project/MyClass.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace project
+{
+    class MyClass : IInterface1, IInterface2
+    {
+        void IInterface1.Method()
+        {
+
+        }
+
+        void IInterface2.Method()
+        {
+
+        }
+
+        public void Method()
+        {
+
+        }
+    }
+}
---
+++ project/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace project
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+
+    interface IInterface1
+    {
+   void Method();
+    }
+
+    interface IInterface2
+    {
+        void Method();
+    }    
+}
